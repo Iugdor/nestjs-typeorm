852f1d30c7e29e8f2b9f42185738c49644824393
Iugdor <grodasgomez@gmail.com>
2021-05-28 14:44:01 -0400

    Pleas work

--- ormconfig.js
+++ ormconfig.js
@@ -1,20 +1,15 @@
-const rootDir = process.env.NODE_ENV === 'prod' ? 'dist' : 'src';
-console.log(process.env.NODE_ENV);
 module.exports = {
   type: 'postgres',
   url: process.env.DATABASE_URL,
   synchronize: false,
   logging: false,
-  migrations: [`${rootDir}/database/migrations/*.ts`],
+  migrations: ['src/database/migrations/*.ts'],
   migrationsTableName: 'migrations',
-  entities: [`${rootDir}/**/*.entity.ts`],
+  entities: ['src/**/*.entity.ts'],
   cli: {
-    migrationsDir: `${rootDir}/database/migrations`,
+    migrationsDir: 'src/database/migrations',
   },
-  ssl:
-    process.env.NODE_ENV === 'prod'
-      ? {
-          rejectUnauthorized: false,
-        }
-      : null,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 };
